79528d9b6ed3190c2fb68d0cc312df7bf684119f
Sahil Singh <hawkinsaspacealien@gmail.com>
2022-08-01 14:24:54 +0530

    another commit

--- src/main/java/com/example/controller/DemoController.java
+++ src/main/java/com/example/controller/DemoController.java
@@ -2,10 +2,15 @@
 
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
-	
+	@RequestMapping(value="/")
+	public String method1()
+	{
+		return("hello there");
+	}
 }
